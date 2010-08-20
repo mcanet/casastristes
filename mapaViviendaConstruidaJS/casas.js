ade06b8480fc6b4b395886115540ec952495064d
Gerald Kogler <geraldo@servus.at>
2010-08-20 07:42:29 +0200

    visualization mapaViviendaConstruidaJS added

---
+++ mapaViviendaConstruidaJS/casas.js
@@ -0,0 +1,165 @@
+{
+	"items" : [
+		{
+			"label" : "MADRID",
+			"type" :  "Item",
+			"final" :  "61621",
+			"LUGAR" : "MADRID",
+			"principio" :  "13853"
+		},
+		{
+			"label" : "CEUTA",
+			"type" :  "Item",
+			"final" :  "174",
+			"LUGAR" : "CEUTA",
+			"principio" :  "554"
+		},
+		{
+			"label" : "BALEARES",
+			"type" :  "Item",
+			"final" :  "10172",
+			"LUGAR" : "BALEARES",
+			"principio" :  "13315"
+		},
+		{
+			"label" : "ASTURIAS",
+			"type" :  "Item",
+			"final" :  "12764",
+			"LUGAR" : "ASTURIAS",
+			"principio" :  "4283"
+		},
+		{
+			"label" : "ARAGON",
+			"type" :  "Item",
+			"final" :  "12865",
+			"LUGAR" : "ARAGON",
+			"principio" :  "4873"
+		},
+		{
+			"label" : "MURCIA",
+			"type" :  "Item",
+			"final" :  "41876",
+			"LUGAR" : "MURCIA",
+			"principio" :  "12534"
+		},
+		{
+			"label" : "CANTABRIA",
+			"type" :  "Item",
+			"final" :  "8864",
+			"LUGAR" : "CANTABRIA",
+			"principio" :  "1460"
+		},
+		{
+			"label" : "EXTREMADURA",
+			"type" :  "Item",
+			"final" :  "7621",
+			"LUGAR" : "EXTREMADURA",
+			"principio" :  "1449"
+		},
+		{
+			"label" : "GALICIA",
+			"type" :  "Item",
+			"final" :  "29998",
+			"LUGAR" : "GALICIA",
+			"principio" :  "10661"
+		},
+		{
+			"label" : "CASTILLA LA MANCHA",
+			"type" :  "Item",
+			"final" :  "36088",
+			"LUGAR" : "CASTILLA LA MANCHA",
+			"principio" :  "6606"
+		},
+		{
+			"label" : "RIOJA",
+			"type" :  "Item",
+			"final" :  "5434",
+			"LUGAR" : "RIOJA",
+			"principio" :  "1883"
+		},
+		{
+			"label" : "ANDALUCIA",
+			"type" :  "Item",
+			"final" :  "123799",
+			"LUGAR" : "ANDALUCIA",
+			"principio" :  "45099"
+		},
+		{
+			"label" : "NAVARRA",
+			"type" :  "Item",
+			"final" :  "5900",
+			"LUGAR" : "NAVARRA",
+			"principio" :  "2390"
+		},
+		{
+			"label" : "VALENCIA",
+			"type" :  "Item",
+			"final" :  "84813",
+			"LUGAR" : "VALENCIA",
+			"principio" :  "48612"
+		},
+		{
+			"label" : "CATALUÑA",
+			"type" :  "Item",
+			"final" :  "80817",
+			"LUGAR" : "CATALUÑA",
+			"principio" :  "30526"
+		},
+		{
+			"label" : "MELILLA",
+			"type" :  "Item",
+			"final" :  "514",
+			"LUGAR" : "MELILLA",
+			"principio" :  "243"
+		},
+		{
+			"label" : "CASTILLA LEON",
+			"type" :  "Item",
+			"final" :  "38232",
+			"LUGAR" : "CASTILLA LEON",
+			"principio" :  "10759"
+		},
+		{
+			"label" : "CANARIAS",
+			"type" :  "Item",
+			"final" :  "26286",
+			"LUGAR" : "CANARIAS",
+			"principio" :  "12962"
+		},
+		{
+			"label" : "PAIS VASCO",
+			"type" :  "Item",
+			"final" :  "9794",
+			"LUGAR" : "PAIS VASCO",
+			"principio" :  "5908"
+		},
+		
+			{ id: 'MADRID', LUGARLatLng: '40.416741,-3.70325' },
+			{ id: 'CEUTA', LUGARLatLng: '35.888287,-5.316195' },
+			{ id: 'BALEARES', LUGARLatLng: '39.56950658,2.647447' },
+			{ id: 'ASTURIAS', LUGARLatLng: '43.357964, -5.873386' },
+			{ id: 'ARAGON', LUGARLatLng: '41.648288,-0.883026' }, 
+			{ id: 'MURCIA', LUGARLatLng: '37.983375,-1.129897' },
+			{ id: 'CANTABRIA', LUGARLatLng:  '43.472247,-3.819935' },
+			{ id: 'EXTREMADURA', LUGARLatLng:  '39.483333,-6.366666' },
+			{ id: 'GALICIA', LUGARLatLng: '42.876467,-8.54599' },
+			{ id: 'CASTILLA LA MANCHA', LUGARLatLng: '39.867653,-4.009878' }, 
+			{ id: 'RIOJA', LUGARLatLng: '36.944059,-2.462424' },
+			{ id: 'ANDALUCIA', LUGARLatLng: ' 37.883333,-4.766666' },
+			{ id: 'NAVARRA', LUGARLatLng: '41.602953,-4.766666' },
+			{ id: 'VALENCIA', LUGARLatLng: '39.456116,-0.354566' },
+			{ id: 'CATALUÑA', LUGARLatLng: '41.384022,2.174606' },
+			{ id: 'MELILLA', LUGARLatLng: '35.291937,-2.938484' },
+			{ id: 'CASTILLA LEON', LUGARLatLng: '42.599876,-5.571752' },
+			{ id: 'CANARIAS', LUGARLatLng: '28.116543,-15.438976' }, 
+			{ id: 'PAIS VASCO', LUGARLatLng: '43.262706,-2.925281' }, 
+	],
+	properties: {
+        'principio': {
+            valueType:              "number"
+        },
+        'final': {
+            valueType:              "number"
+        }
+    }
+}
